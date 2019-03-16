27f24adf64529bf58a9f52a54efe6f58e6cc966e
Mateja Petrovic <mateja176@gmail.com>
2019-03-16 12:52:02 +0100

    Adds loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { merge, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loading$: Observable<boolean>;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const start$ = this.router.events.pipe(
+      filter(e => e instanceof NavigationStart),
+      map(() => true),
+    );
+    const end$ = this.router.events.pipe(
+      filter(
+        e =>
+          e instanceof NavigationEnd ||
+          e instanceof NavigationCancel ||
+          e instanceof NavigationError,
+      ),
+      map(() => false),
+    );
+
+    this.loading$ = merge(start$, end$);
+  }
+}
